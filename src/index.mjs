b5a507feaa885115e20b405d55e7e2b453a289b1
GakPower <alanigogos@gmail.com>
2020-10-10 20:50:22 +0200

    cleared code

--- src/index.mjs
+++ src/index.mjs
@@ -21,12 +21,6 @@ app.use((req, res, next) => {
 		'Access-Control-Allow-Methods',
 		'GET, POST, OPTIONS, PUT, PATCH, DELETE'
 	);
-	// res.header(
-	// 	'Access-Control-Allow-Methods',
-	// 	'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-	// );
-	// res.header('Access-Control-Allow-Headers', 'Content-Type');
-	// res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader(
 		'Access-Control-Allow-Headers',
 		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
