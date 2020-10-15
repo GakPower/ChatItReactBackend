9df3d71cc3074ab0bce893bfc1a7a1f0fba96fdc
GakPower <alanigogos@gmail.com>
2020-10-15 23:46:05 +0200

    changed validation so that username is not valid email

--- src/validation.mjs
+++ src/validation.mjs
@@ -4,9 +4,9 @@ import bcrypt from 'bcrypt';
 
 const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/i;
 
-const usernameSchema = Joi.string().alphanum().required().max(255);
+const usernameSchema = Joi.string().required().not('@').max(255);
 const emailSchema = Joi.string().required().pattern(emailRegex).max(255);
-const passwordSchema = Joi.string().alphanum().required().min(8).max(1024);
+const passwordSchema = Joi.string().required().min(8).max(1024);
 
 const joinSchema = Joi.object({
 	username: usernameSchema,
