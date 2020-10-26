0e4afbcd9e2d59d57dafa81ff1e2872e11e7f65c
GakPower <alanigogos@gmail.com>
2020-10-27 00:08:47 +0100

    removed the http server, fixed an issue with socket.io and finalized the app for production

--- src/index.mjs
+++ src/index.mjs
@@ -4,7 +4,9 @@ import dotenv from 'dotenv';
 import authRoutes from './routes/auth.mjs';
 import appRoutes from './routes/app.mjs';
 import socketIO from 'socket.io';
-import httpServer from 'http';
+import https from 'https';
+import fs from 'fs';
+import path from 'path';
 
 dotenv.config();
 
@@ -13,10 +15,8 @@ Mongoose.connect(process.env.DB_CONNECT, { useUnifiedTopology: true }, () =>
 );
 
 const app = Express();
-const http = httpServer.createServer(app);
 
 app.use(Express.json());
-
 app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Allow-Credentials', 'true');
@@ -32,12 +32,30 @@ app.use((req, res, next) => {
 });
 app.use('/auth', authRoutes);
 app.use('/app', appRoutes);
+app.use(Express.static('build'));
+app.get('/*', function (req, res) {
+	res.sendFile(path.join(path.resolve(), './build/index.html'), function (err) {
+		if (err) {
+			res.status(500).send(err);
+		}
+	});
+});
 
-const io = socketIO(http);
-io.on('connection', (socket) => {
+const httpsServer = https.createServer(
+	{
+		key: fs.readFileSync('./chatit.site.key'),
+		cert: fs.readFileSync('./chatit.site.pem'),
+	},
+	app
+);
+
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
+});
+
+const io = socketIO.listen(httpsServer);
+io.sockets.on('connection', (socket) => {
 	socket.on('message', (data) => {
 		socket.broadcast.emit('message', data);
 	});
 });
-
-http.listen(5000, () => console.log('Server is running'));
